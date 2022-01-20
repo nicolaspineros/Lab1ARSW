eb2bba419d77ad97e226c2fa4a9c2bc7e070b105
Nicolas Piñeros Campo <onpc1224@gmail.com>
2022-01-20 15:33:16 +0100

    first commit

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,18 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread extends Thread{
+
+    private int a,b;
+
+    public CountThread(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public void run(){
+        for (int i=a; i<=b; i++){
+            System.out.println(i);
+        }
+    }
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -10,9 +10,14 @@ package edu.eci.arsw.threads;
  * @author hcadavid
  */
 public class CountThreadsMain {
-    
+
     public static void main(String a[]){
-        
+        CountThread hilo1 = new CountThread(0,99);
+        CountThread hilo2 = new CountThread(100,199);
+        CountThread hilo3 = new CountThread(200,299);
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
     }
     
 }
